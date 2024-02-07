88988bc7c6d1dd64ca520fecc048d29b95184111
unknown <SenaManana@ARMAPRCCIFOD594.FormacionSena.RED>
2024-02-07 11:37:32 -0500

    Inicio del home 2.0

--- src/Homepage/FrontPage.tsx
+++ src/Homepage/FrontPage.tsx
@@ -5,7 +5,7 @@ const FrontPage = () => {
   return (
     <section className='container-father-page'>
       <div className="container-text">
-        <h1 className='title-page'>MULTISERVICIOS COMPANY</h1>
+        <h1 className='title-page'>MULTISERVICIOS COMPANY </h1>
         <h2 className='subTitle-page'>WEB DESIGN</h2>
         <button>FUNCIONA?</button>
       </div>
